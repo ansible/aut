4185218ed8cfe3ac47e4378901ee6c2816d4eece
Rick Elrod <rick@elrod.me>
2020-12-11 20:53:40 -0600

    just sudo on osx
    Signed-off-by: Rick Elrod <rick@elrod.me>

--- pre/pip.sh
+++ pre/pip.sh
@@ -12,4 +12,9 @@ if [[ $? -ne 0 ]]; then
   PIP="$(which pip-3)"
 fi
 
+# On macOS we need to sudo. On everything else, we run as root.
+if [ "$(uname)" == "Darwin" ]; then
+  PIP="sudo $PIP"
+fi
+
 $PIP install $PRODUCT==$VERSION

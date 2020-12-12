cc770a25cfee9e201a9493db64345933e41cf072
Rick Elrod <rick@elrod.me>
2020-12-11 23:53:34 -0600

    more osx fixes?
    Signed-off-by: Rick Elrod <rick@elrod.me>

--- pre/pip.sh
+++ pre/pip.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -xu
+
 # See what pip is called on the platform
 
 PIP="$(which pip)"
@@ -13,8 +15,10 @@ if [[ $? -ne 0 ]]; then
 fi
 
 # On macOS we need to sudo. On everything else, we run as root.
-if [ "$(uname)" == "Darwin" ]; then
+if [[ "$(uname -s)" == "Darwin" ]]; then
   PIP="sudo $PIP"
 fi
+
+set -e
 
 $PIP install $PRODUCT==$VERSION
--- pre/tar.sh
+++ pre/tar.sh
@@ -16,9 +16,24 @@ set -e
 
 wget "https://releases.ansible.com/$PRODUCT/$PRODUCT-$VERSION.tar.gz"
 tar -xvf "$PRODUCT-$VERSION.tar.gz"
-ln -s "$(pwd)/$PRODUCT-$VERSION"/bin/* /usr/bin/
 
-$PIP install jinja2 pyyaml
+set +e
+
+# On macOS we need to sudo. On everything else, we run as root.
+if [[ "$(uname -s)" == "Darwin" ]]; then
+  set -e
+  sudo $PIP install jinja2 pyyaml
+  #sudo ln -s "$(pwd)/$PRODUCT-$VERSION"/bin/* /usr/local/bin/
+  pushd "$(pwd)/$PRODUCT-$VERSION"
+  sudo python3 setup.py install
+  popd
+  set +e
+else
+  set -e
+  $PIP install jinja2 pyyaml
+  ln -s "$(pwd)/$PRODUCT-$VERSION"/bin/* /usr/bin/
+  set +e
+fi
 
 set +e
 
@@ -26,11 +41,11 @@ set +e
 which python
 
 if [[ $? -ne 0 ]]; then
-    which python3
-    if [[ $? -eq 0 ]]; then
-        ln -s "$(which python3)" /usr/bin/python
-    else
-        echo "No python/python3 found"
-        exit 1
-    fi
+  which python3
+  if [[ $? -eq 0 ]]; then
+    ln -s "$(which python3)" /usr/bin/python
+  else
+    echo "No python/python3 found"
+    exit 1
+  fi
 fi

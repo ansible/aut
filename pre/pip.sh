5f33d7c4c386bf3ea8dc259dc9147b69de165364
Rick Elrod <rick@elrod.me>
2020-12-11 20:51:54 -0600

    Revert "pass --user to pip on macos"
    This reverts commit 91f679d25cfe66f710ea5177d873651f184b030d.

--- pre/pip.sh
+++ pre/pip.sh
@@ -12,10 +12,4 @@ if [[ $? -ne 0 ]]; then
   PIP="$(which pip-3)"
 fi
 
-# On macOS we want to install with --user. On everything else, we run as root.
-
-if [[ "$(uname -s)" == "Darwin" ]]; then
-  $PIP install --user $PRODUCT==$VERSION
-else
-  $PIP install $PRODUCT==$VERSION
-fi
+$PIP install $PRODUCT==$VERSION

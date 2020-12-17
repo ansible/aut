f34611d15a4129cec338af86f55dea0f6a16d51d
Rick Elrod <rick@elrod.me>
2020-12-16 23:24:05 -0600

    fix PATH for macos
    Signed-off-by: Rick Elrod <rick@elrod.me>

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,14 @@ set -eux
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+# Some systems (cough, osx) need their $PATH altered for certain pre's.
+# Just do it here so they affect all run-tests.
+export PATH="$PATH:$(python3 -c 'import sysconfig; print(sysconfig.get_paths()["data"])')/bin"
+
 cd "$DIR/pre"
 bash "${PRE}.sh"
 
 cd ../run
 for file in *.sh; do
-    bash "$file"
+  bash "$file"
 done

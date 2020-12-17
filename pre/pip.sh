b3349be655e985d010ba77894325a04469ca6719
Rick Elrod <rick@elrod.me>
2020-12-16 23:33:27 -0600

    try preferring pip3?
    Signed-off-by: Rick Elrod <rick@elrod.me>

--- pre/pip.sh
+++ pre/pip.sh
@@ -4,14 +4,14 @@ set -xu
 
 # See what pip is called on the platform
 
-PIP="$(which pip)"
-
-if [[ $? -ne 0 ]]; then
-  PIP="$(which pip3)"
-fi
+PIP="$(which pip3)"
 
 if [[ $? -ne 0 ]]; then
   PIP="$(which pip-3)"
+fi
+
+if [[ $? -ne 0 ]]; then
+  PIP="$(which pip)"
 fi
 
 # On macOS we need to sudo. On everything else, we run as root.
